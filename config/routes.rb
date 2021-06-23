62763232ba94547e33eb5c8cea4900231b57033b
Irving Henriquez <henriquezirv@gmail.com>
2021-06-23 20:28:50 +0000

    added user and session controllers with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+ 
+
+  #sessions
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+   # sign-up
+   get '/signup' => 'users#new'
+   post '/users' => 'users#create'
+
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]

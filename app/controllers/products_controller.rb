1fd0e1c8b53d0c7d4cf5075c4a01516f97eebca5
Irving Henriquez <henriquezirv@gmail.com>
2021-06-23 01:16:59 +0000

    added sold-out badge feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,12 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    
   end
 
   def show
     @product = Product.find params[:id]
+    
   end
 
 end
